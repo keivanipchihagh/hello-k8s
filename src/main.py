aeb22a93e1bd98a576f1785adb47308c13556b19
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2023-09-10 15:37:54 +0330

    feat: add Flask source code

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+APP = Flask(__name__)
+load_dotenv()
+
+
+@APP.route("/")
+def hello():
+  return render_template ("index.html")
+
+
+
+if __name__ == '__main__':
+    APP.run(
+        host = os.getenv('HOST', '0.0.0.0'),
+        port = os.getenv('PORT', 80),
+        debug = True
+    )
